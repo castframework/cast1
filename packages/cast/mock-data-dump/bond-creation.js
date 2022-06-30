2734e6abc28d84cb129e0eae69f8de8bbeacaf88
David Boclé <david.bocle@finastra.com>
2022-06-30 16:02:05 +0200

    synchronous bond subscription

--- packages/cast/mock-data-dump/bond-creation.js
+++ packages/cast/mock-data-dump/bond-creation.js
@@ -14,7 +14,7 @@ const mockInitiateSubscriptionInput = require('./queries-variables/initiateSubsc
 const sub = require('./subscription');
 const { v4: uuid } = require('uuid');
 const investors = require('./mock-investors.json');
-const issuerAccountNumber = generateIban();
+const {subscribeBond} = require('./bond-subscription');
 
 exports.forgeBond = async (froClient, froAddress, fsoClient, fsoAddress, ledger, bondToForge) => {
     return new Promise(async (resolve, reject) => {
@@ -31,6 +31,7 @@ exports.forgeBond = async (froClient, froAddress, fsoClient, fsoAddress, ledger,
             await froClient.request(CreateBond, { bond });
         } catch (err) {
             reject(err);
+            return;
         }
 
         const registrySub = sub.create(REGISTRY_NOTIFICATION_SUB).subscribe(async (event) => {
@@ -38,66 +39,18 @@ exports.forgeBond = async (froClient, froAddress, fsoClient, fsoAddress, ledger,
             console.log('Instrument created:', instrumentAddress);
 
             console.log('Initiate Subscriptions for bond:', instrumentAddress);
-            const issuerLEI = bondToForge.issuerId;
             for (const [index, investor] of investors.entries()) {
-                let initiateSubscriptionInput = {
-                    ...mockInitiateSubscriptionInput,
-                    ...{
-                        'issuerAddresses': {
-                            'legalEntityId': issuerLEI,
-                            'paymentAccountNumber': issuerAccountNumber
-                        }
-                    },
-                    ...investor,
-                    instrumentAddress,
-                    operationId: uuid(),
-                    tradeId: uuid(),
-                    instrumentLedger: ledger
-                };
-                await froClient.request(InitiateSubscription, { initiateSubscriptionInput });
                 console.log(`Subscription ${index + 1}/${investors.length}`);
+                try {
+                    await subscribeBond(froClient, fsoClient, ledger, instrumentAddress, investor, bondToForge.issuerId);
+                } catch (err) {
+                    reject(err);
+                }
+                console.log(`Subscription ${index + 1}/${investors.length} DONE !`);
             };
             registrySub.unsubscribe();
+            resolve();
         });
 
-        const contractSub = sub.create(CONTRACT_NOTIFICATION_SUB).subscribe(async (event) => {
-            if (event.data.contractNotification.notificationName === 'SubscriptionInitiated') {
-                const getSettlementTransactionId =
-                    event.data.contractNotification.lightSettlementTransactions[0].id;
-                console.log('Settlement transaction Id:', getSettlementTransactionId);
-
-                const settlementTransaction = await fsoClient.request(GetSettlementTransaction, {
-                    getSettlementTransactionId,
-                });
-                const firstMovementRef =
-                    settlementTransaction.getSettlementTransaction.movements[0].paymentReference;
-                const secondMovementRef =
-                    settlementTransaction.getSettlementTransaction.movements[1].paymentReference;
-                console.log(
-                    'Settlement Status:',
-                    settlementTransaction.getSettlementTransaction.settlementStatus
-                );
-
-                await fsoClient.request(ConfirmPaymentReceived, {
-                    paymentReference: firstMovementRef,
-                });
-                console.log('Settlement Status: PROCESSED');
-
-                await fsoClient.request(ConfirmPaymentTransferred, {
-                    paymentReference: secondMovementRef,
-                });
-                console.log('Settlement Status: SETTLED');
-                contractSub.unsubscribe();
-                resolve();
-            }
-        });
     })
-}
-
-function generateIban() {
-    let rdx = "";
-    for (let index = 0; index < 11; index++) {
-        rdx += Math.floor(Math.random() * 10).toString();
-    }
-    return "FR76300030113004" + rdx;
 }
